95b9bf6ea5183cf1cf7aae1db477ef99de0fd77f
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-02 14:41:07 -0500

    feat hero

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -33,10 +33,12 @@ export const HeroSection = () => {
           </p>
 
           <div className="space-y-4 md:space-y-0 md:space-x-4">
-            <Button className="w-5/6 md:w-1/4 font-bold group/arrow  bg-[#ed9238] hover:bg-[#ffa347da]">
-              Probar YachayBot
-              <ArrowRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
-            </Button>
+          <Link href="/ai-bot">
+  <Button className="w-5/6 md:w-1/4 font-bold group/arrow  bg-[#ed9238] hover:bg-[#ffa347da]">
+    Probar YachayBot
+    <ArrowRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
+  </Button>
+</Link>
           </div>
         </div>
 
