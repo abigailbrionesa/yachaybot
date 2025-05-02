5ac604d855aefeb67b6a98155c117b3c39807b52
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-02 06:25:56 -0500

    remove approutercacheprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 
 import "./globals.css";
 
@@ -29,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AppRouterCacheProvider>
         {children}
-        </AppRouterCacheProvider>
       </body>
     </html>
   );
