ca811a648cd3346cee6537ba4f1e39085d24228b
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-02 13:25:12 -0500

    create navbar

---
+++ src/components/global/navbar.tsx
@@ -0,0 +1,159 @@
+"use client";
+import { ChevronsDown, Github, Menu } from "lucide-react";
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import { Separator } from "../ui/separator";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "../ui/navigation-menu";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import { ToggleTheme } from "./toogle-theme";
+
+interface RouteProps {
+  href: string;
+  label: string;
+}
+
+interface FeatureProps {
+  title: string;
+  description: string;
+}
+
+const routeList: RouteProps[] = [
+  {
+    href: "#testimonials",
+    label: "Testimonials",
+  },
+  {
+    href: "#team",
+    label: "Team",
+  },
+  {
+    href: "#contact",
+    label: "Contact",
+  },
+  {
+    href: "#faq",
+    label: "FAQ",
+  },
+];
+
+const featureList: FeatureProps[] = [
+  {
+    title: "Showcase Your Value ",
+    description: "Highlight how your product solves user problems.",
+  },
+  {
+    title: "Build Trust",
+    description:
+      "Leverages social proof elements to establish trust and credibility.",
+  },
+  {
+    title: "Capture Leads",
+    description:
+      "Make your lead capture form visually appealing and strategically.",
+  },
+];
+
+export const Navbar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <header className="shadow-inner bg-opacity-15
+    lg:max-w-screen-xl top-5 mx-auto sticky border border-secondary z-40 rounded-2xl flex justify-between items-center p-2 bg-card">
+      <Link href="/" className="font-bold text-lg flex items-center">
+        <Image src="/images/logo/yachaybot_logo2.png" alt="logo" width={100} height={100} className="h-auto w-auto max-w-10" />
+        YachayBot
+      </Link>
+      {/* <!-- Mobile --> */}
+      <div className="flex items-center lg:hidden">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild>
+            <Menu
+              onClick={() => setIsOpen(!isOpen)}
+              className="cursor-pointer lg:hidden"
+            />
+          </SheetTrigger>
+
+          <SheetContent
+            side="left"
+            className="flex flex-col justify-between rounded-tr-2xl rounded-br-2xl bg-card border-secondary"
+          >
+            <div>
+              <SheetHeader className="mb-4 ml-4">
+                <SheetTitle className="flex items-center">
+                  <Link href="/" className="flex items-center">
+                    <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
+                    Shadcn
+                  </Link>
+                </SheetTitle>
+              </SheetHeader>
+
+              <div className="flex flex-col gap-2">
+                {routeList.map(({ href, label }) => (
+                  <Button
+                    key={href}
+                    onClick={() => setIsOpen(false)}
+                    asChild
+                    variant="ghost"
+                    className="justify-start text-base"
+                  >
+                    <Link href={href}>{label}</Link>
+                  </Button>
+                ))}
+              </div>
+            </div>
+
+            <SheetFooter className="flex-col sm:flex-col justify-start items-start">
+              <Separator className="mb-2" />
+
+              <ToggleTheme />
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
+      </div>
+
+      {/* <!-- Desktop --> */}
+      <NavigationMenu className="hidden lg:block mx-auto">
+        <NavigationMenuList className="flex ">
+          <NavigationMenuItem className="flex">
+            {routeList.map(({ href, label }) => (
+              <NavigationMenuLink key={href} asChild>
+                <Link href={href} className="text-base px-2">
+                  {label}
+                </Link>
+              </NavigationMenuLink>
+            ))}
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <div className="hidden lg:flex">
+        <ToggleTheme />
+
+        <Button asChild size="sm" variant="ghost" aria-label="View on GitHub">
+          <Link
+            aria-label="View on GitHub"
+            href="https://github.com/abigailbrionesa/yachaybot"
+            target="_blank"
+          >
+            <Github className="size-5" /> Ver en Github
+          </Link>
+        </Button>
+      </div>
+    </header>
+  );
+};
