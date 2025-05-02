568e414aff3a0b7e7042908eb429127809531c0a
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-02 09:09:38 -0500

    add client layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import ClientLayout from "./client-layout";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ClientLayout>
         {children}
+        </ClientLayout>
       </body>
     </html>
   );
