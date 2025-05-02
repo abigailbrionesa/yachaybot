d42d976898b944e3c8b2576b82a28cf61215ad77
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-02 07:23:33 -0500

    api route.ts to authenticate

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,80 @@
+import { prisma } from '@/lib/prisma'
+import { getUserSession } from '@/lib/session'
+import { NextAuthOptions } from 'next-auth'
+import NextAuth from 'next-auth/next'
+import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!
+
+const authOption: NextAuthOptions = {
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch(`${process.env.NEXTAUTH_URL}/your/endpoint`, {
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(credentials),
+        })
+
+        const user = await res.json()
+
+        if (res.ok && user) {
+          return user
+        }
+        return null
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account?.provider === 'google') {
+        if (!profile?.email) throw new Error('No profile')
+
+        await prisma.user.upsert({
+          where: { email: profile.email },
+          create: { email: profile.email, name: profile.name },
+          update: { name: profile.name },
+        })
+      }
+
+      return true
+    },
+    async session({ session, token }) {
+      const userSession = await getUserSession()
+      session.user = userSession || {}
+      return session
+    },
+    async jwt({ token, user, account, profile }) {
+      if (profile?.email) {
+        const existingUser = await prisma.user.findUnique({
+          where: { email: profile.email },
+        })
+        if (!existingUser) throw new Error('No user found')
+        token.id = existingUser.id
+      }
+
+      if (user) {
+        token.id = (user as any).id
+      }
+
+      return token
+    },
+  },
+}
+
+const handler = NextAuth(authOption)
+export { handler as GET, handler as POST }
