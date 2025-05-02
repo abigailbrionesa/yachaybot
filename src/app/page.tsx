ae93fe77c4ab4465bcbe454cd11ff909a477f9fb
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-02 07:18:22 -0500

    preview signin component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import SignIn from "@/components/sections/signIn/SignIn";
 export default function Home() {
   return (
-    <div>
-      hola
-      <Button>Click me</Button>
-    </div>
-  );
+
+
+  <SignIn />)
+  
+  ;
 }
