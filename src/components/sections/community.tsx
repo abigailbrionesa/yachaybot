3612f5b0fe44d427fad527da33831ad470da2320
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-02 14:28:29 -0500

    put bg image

--- src/components/sections/community.tsx
+++ src/components/sections/community.tsx
@@ -9,21 +9,26 @@ import {
 
 export const CommunitySection = () => {
   return (
-    <section id="community" className="py-12 ">
+    <section id="community" style={{
+      backgroundImage: 'url("https://canal21huancayo.com/wp-content/uploads/2023/07/WhatsApp-Image-2023-07-17-at-5.38.07-PM-2-1024x614.jpeg")',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+    }}>
       <hr className="border-secondary" />
       <div className="container py-20 sm:py-20">
         <div className="lg:w-[60%] mx-auto">
-          <Card className="bg-background border-none shadow-none text-center flex flex-col items-center justify-center">
-            <CardHeader>
-              <CardTitle className="text-4xl md:text-5xl font-bold flex flex-col items-center">
-                <div>
-                  ¿Listo para ser guardian@ de los
-                  <span className="text-transparent pl-2 bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text">
-                    saberes ancestrales?
-                  </span>
-                </div>
-              </CardTitle>
-            </CardHeader>
+          <Card className="bg-background 
+          
+          border-none shadow-none text-center flex flex-col items-center justify-center">
+
+            <h2>
+              ¿Listo para ser guardian@ de los {" "}
+              <span className="text-transparent bg-gradient-to-r from-[#31bd46] to-primary bg-clip-text">
+                saberes ancestrales?
+              </span>
+            </h2>
+
             <CardContent className="lg:w-[80%] text-xl text-muted-foreground">
               Únete a nuestra red intercultural de docentes, jóvenes y sabios quechuas/aimaras. Comparte conocimientos, resuelve dudas y ayuda a preservar nuestra herencia. ¡Tu voz importa! 🌱
             </CardContent>
