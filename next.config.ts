f3051cd3cc60064ad0ec1426ef703d24c399546c
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-02 14:27:21 -0500

    accept images from these domains

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['i.pravatar.cc', "images.unsplash.com", "avatars.githubusercontent.com"],
+    domains: ['i.pravatar.cc', "nuestrodesafioclimatico.minam.gob.pe", "images.unsplash.com", "avatars.githubusercontent.com"],
   },
 };
 
