9aad3a371246ec46d60f3e1bc40eb3f7b5411d53
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-02 20:13:39 -0500

    do not redirect user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,15 +13,10 @@ import { ContactSection } from "@/components/sections/contact";
 import { FAQSection } from "@/components/sections/faq";
 import { FooterSection } from "@/components/sections/footer";
 import { Navbar } from "@/components/global/navbar";
+
 export default function MainPage() {
   const { status } = useSession();
   const router = useRouter();
-
-  useEffect(() => {
-    if (status === "authenticated") {
-      router.replace("/ai-bot");
-    }
-  }, [status]);
 
   if (status === "loading") {
     return (
